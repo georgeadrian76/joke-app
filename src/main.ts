97fc5b05903701a2a075693cbef3f1908f51788e
georgeadrian76 <georgeadrian76.com>
2024-03-26 18:57:44 +0200

    understanding the logic

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,31 @@ categoryForm.addEventListener("submit", async (event) => {
   event.preventDefault(); // Prevent default form submission behavior
 
   // Retrieve selected categories
+
+  const inputEl = document.getElementsByTagName("input")[0];
+  const test = inputEl.value;
+  console.log(test);
+
+  // categories.forEach((e) => console.log(e.value));
+
   const selectedCategories: Category[] = [];
 
   const joke = await fetchJoke(selectedCategories);
+  // const category = joke.category;
+
+  selectedCategories.push(joke.category);
+  console.log(selectedCategories);
 
   displayJoke(joke);
 });
 
 // Function to display joke
 function displayJoke(joke: SingleJoke | TwoPartJoke) {
+  const test = joke.type;
+
+  console.log(test);
   // TODO: Implement a feature to blur flagged jokes and provide a button to unblur them.
+  const div = document.createElement("div");
+  div.innerHTML = `${test}`;
+  jokeElement.appendChild(div);
 }
